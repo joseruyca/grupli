76fdb7b16fc226311299722fc5539c515b02a82e
Jose Rubio <joseruyca@gmail.com>
2026-06-18 11:08:18 +0200

    Use committed Flutter web build for Vercel

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1765530968" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "447244465" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
