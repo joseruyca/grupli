8c507f2fa3400c5ab6234a8e5642e7c15468af59
Jose Rubio <joseruyca@gmail.com>
2026-06-18 10:47:45 +0200

    Fix Cupertino transition import for Vercel

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'dart:math';
 import 'dart:ui' as ui;
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:app_links/app_links.dart';
@@ -121,7 +122,7 @@ Future<void> main() async {
 }
 
 class AppConfig {
-  static const appVersion = 'v16.32';
+  static const appVersion = 'v16.32.3';
   static const enableRealtimeSubscriptions = false;
 
   // Security baseline:
